60cc2c333af461c0b1657bf45ad8af827da06f2d
mouyigang <mouyigang@gmail.com>
2015-10-16 18:39:28 +0800

    made a stupid mistake

--- jenkins.sh
+++ jenkins.sh
@@ -1,16 +1,36 @@
-FROM jenkins:latest
+#! /bin/bash
 
-USER root
-RUN apt-get update && apt-get install -y sudo lxc && rm -rf /var/lib/apt/lists/*
-RUN echo "jenkins ALL=NOPASSWD: ALL" >> /etc/sudoers
+set -e
 
-#Install docker
-RUN curl -sSL https://get.docker.com/ubuntu/ | sh
-RUN mkdir /data
-RUN chown -R jenkins /data
+#make sure jenkins can run docker command
+sudo groupmod -g $(stat -c "%g" /var/run/docker.sock) jenkins
+# Copy files from /usr/share/jenkins/ref into /var/jenkins_home
+# So the initial JENKINS-HOME is set with expected content.
+# Don't override, as this is just a reference setup, and use from UI
+# can then change this, upgrade plugins, etc.
+copy_reference_file() {
+	f=${1%/}
+	echo "$f" >> $COPY_REFERENCE_FILE_LOG
+    rel=${f:23}
+    dir=$(dirname ${f})
+    echo " $f -> $rel" >> $COPY_REFERENCE_FILE_LOG
+	if [[ ! -e /var/jenkins_home/${rel} ]]
+	then
+		echo "copy $rel to JENKINS_HOME" >> $COPY_REFERENCE_FILE_LOG
+		mkdir -p /var/jenkins_home/${dir:23}
+		cp -r /usr/share/jenkins/ref/${rel} /var/jenkins_home/${rel};
+		# pin plugins on initial copy
+		[[ ${rel} == plugins/*.jpi ]] && touch /var/jenkins_home/${rel}.pinned
+	fi;
+}
+export -f copy_reference_file
+echo "--- Copying files at $(date)" >> $COPY_REFERENCE_FILE_LOG
+find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 
-USER jenkins
-RUN mkdir -p /data/jenkins
+# if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
+   exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS "$@"
+fi
 
-COPY jenkins.sh /usr/local/bin/jenkins.sh
-ENTRYPOINT ["/bin/tini", "--", "/usr/local/bin/jenkins.sh"]
+# As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
+exec "$@"h"]
